dd2ba9ea128f07eaa80a2bd6a45faf9d24e73e4c
kanimozhi67 <prettykani67@gmail.com>
2025-12-11 19:14:49 +0400

    .env changes

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,11 @@
 import axios from "axios";
-
+const baseURL =
+  process.env.REACT_APP_API_URL || "https://flairbackend.vercel.app";
+console.log("baseURL:", baseURL);
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // make sure this matches your backend
+  // baseURL: "http://localhost:5000/api",
+  //  // make sure this matches your backend
+  baseURL: `${baseURL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
